21c1a6f736037dc9a1611e2f7c2bcf5979dc56bd
Etec Profº Horácio Augusto da Silveira <giovana Monteiro>
2024-02-26 22:21:59 -0300

    feat: add initial project

---
+++ testepam/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace testepam
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ testepam/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace testepam
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string nome = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnNameClicked(object sender, EventArgs e)
+        {
+            nome = NameEntry.Text;
+            GreetingLabel.Text = $"Hello,{nome}";
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+
+
+        }
+            
+    }
+
+}
